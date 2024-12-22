04087ac011d261720a86f925937ff932e8160276
Елена <pox4not@gmail.com>
2024-12-22 11:18:59 +0300

    Простые Юнит-тесты

---
+++ 12Модуль/tests_12_1.py
@@ -0,0 +1,27 @@
+import unittest
+from unittest import TestCase
+from HumanMoveTest.runner import Runner
+
+
+class RunnerTest(TestCase):
+    def test_walk(self):
+        runner = Runner('Оля')
+        for i in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+    def test_run(self):
+        runner = Runner('Оля')
+        for i in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+    def test_challenge(self):
+        runner1 = Runner('Оля')
+        runner2 = Runner('Витя')
+        for i in range(10):
+            runner1.run()
+            runner2.walk()
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+
+if __name__ == '__main__':
+    unittest.main()
